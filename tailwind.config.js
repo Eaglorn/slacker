7ef796af097ac29789b294673d03bce5266a507e
Eaglorn <lexus1.45@mail.ru>
2024-11-19 09:27:09 +1000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/renderer/**/*.{html,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: []
 }
